dc8383b25e8890553dbe7814a4a5ae3497b777e8
Jaudat Syed <sjh.jaudat@gmail.com>
2025-01-19 00:09:33 -0800

    modified hammerspoon keybindings

--- init.lua
+++ init.lua
@@ -4,11 +4,14 @@
 -- App configuration
 APPS = {
 	{ shortcut = "t", name = "Wezterm" },
+	-- { shortcut = "i", name = "IntelliJ IDEA" },
 	{ shortcut = "v", name = "Visual Studio Code" },
 	{ shortcut = "e", name = "Zed" },
-	{ shortcut = "b", name = "Firefox" },
+	-- { shortcut = "g", name = "GitHub Desktop" },
 	{ shortcut = "n", name = "Obsidian" },
+	{ shortcut = "b", name = "Firefox" },
 	-- { shortcut = "c", name = "Slack" },
+	-- { shortcut = "a", name = "Postman" },
 }
 
 -- Bind application shortcuts
